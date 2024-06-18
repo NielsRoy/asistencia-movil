85f736348a50e3f48c489fc04d5040f3beb582ea
NielsRoy <nielsroy8@gmail.com>
2024-06-18 02:30:25 -0400

    url cambia

--- lib/utils/apiBack.dart
+++ lib/utils/apiBack.dart
@@ -1,6 +1,6 @@
 
 // ignore_for_file: file_names
 
-const String apiBack= "https://universidadbackend-examen2.up.railway.app";//"https://asistencia-backend-ebfi.onrender.com";//"http://192.168.0.4:8080";
+const String apiBack= "https://asistencia-backend-ebfi.onrender.com"; //"http://192.168.0.4:8080";"https://universidadbackend-examen2.up.railway.app";
 
-const String apiBackC= "https://universidadbackend-examen2.up.railway.app/auth";//"https://asistencia-backend-ebfi.onrender.com/auth";//"http://192.168.0.2:8080/auth";
+const String apiBackC= "https://asistencia-backend-ebfi.onrender.com/auth";//"http://192.168.0.2:8080/auth";"https://universidadbackend-examen2.up.railway.app/auth";
